2858f6be6660e07b7c2b76e0267add33f60e3395
quthery <chiefkeef7317@gmail.com>
2024-12-21 19:53:28 +0300

    micro fix

--- stream_proxy/cmd/main.go
+++ stream_proxy/cmd/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-  router "UFetch/internal/delivery/http"
 	"fmt"
 	h "net/http"
+	router "roskomtube/internal/delivery/http"
 )
 
 func main() {
-  port := ":8080"
-  fmt.Printf("Started at %s", port)
+	port := ":8080"
+	fmt.Printf("Started at %s", port)
 	router := router.InitAPI()
-  
-  if err := h.ListenAndServe(port , router); err != nil {
+
+	if err := h.ListenAndServe(port, router); err != nil {
 		panic(err)
 	}
 }
